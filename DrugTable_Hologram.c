6ebf8e8c9712d413bf90038124104d74bc95b7c2
Mudarken <91074680+SolexBear@users.noreply.github.com>
2026-02-22 05:41:12 +0100

    Update DrugTable_Hologram.c

--- DrugTable_Hologram.c
+++ DrugTable_Hologram.c
@@ -1,15 +1,1 @@
-// Updated implementation of DrugTable_Hologram.c
 
-// Keep the relevant methods
-
-void DrugTableCollision() {
-    // Implementation here
-}
-
-void ProjectionBasedOnParent() {
-    // Implementation here
-}
-
-void EvaluateCollision() {
-    // Implementation here
-}
